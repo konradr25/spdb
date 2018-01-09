5d1d0827968fabbfc1f903289671a86f811bb465
Konrad Ryczek <konradr25@gmail.com>
2018-01-09 16:49:43 +0100

    Added heatmap Removed ngmap from package.json due to problems with heatmap Added mock points on heatmap Added scope Changed icon

--- frontend/src/app/controllers/main.controller.js
+++ frontend/src/app/controllers/main.controller.js
@@ -1,17 +1,38 @@
 'use strict';
 
-MainController.$inject = ['MainService'];
-function MainController(mainService) {
-    var _this = this;
-    _this.lines = [];
-    _this.allLines = "";
-    _this.loadAllLines = function() {
+MainController.$inject = ['MainService', '$scope'];
+function MainController(mainService, scope) {
+    scope.heatmap = undefined;
+    scope.chosenLine = undefined;
+    scope.startDate = undefined;
+    scope.stopDate = undefined;
+
+    scope.$on('mapInitialized', function(event, map) {
+        scope.heatmap = map.heatmapLayers.foo;
+    });
+
+    scope.allLineDTOs = [];
+    scope.loadAllLines = function() {
         mainService.loadAllLines(function (res) {
-            _this.allLines = _this.mapLines(res);
+            scope.allLineDTOs = mapLines(res);
         });
     };
 
-    _this.mapLines = function(lineNumsArray) {
+    scope.findDelays = function () {
+        mainService.loadDelays(scope.chosenLine, scope.startDate, scope.stopDate, function (res) {
+            scope.selectedPoints = createPoints(res);
+        })
+    };
+
+    scope.querySearch = function (query) {
+        return this.allLineDTOs.filter(function (line) { return line.value === query; });
+    };
+
+    scope.selectedItemChange = function (item) {
+        //TODO
+    }
+
+    var mapLines = function(lineNumsArray) {
         return lineNumsArray.map(function (lineNum) {
             return {
                 value: lineNum,
@@ -20,17 +41,52 @@ function MainController(mainService) {
         });
     };
 
-    _this.querySearch = function (query) {
-        return this.lines.filter(function (line) { return line.value === query; });
+    var createPoints = function (allDelaysDTOs) {
+        return allDelaysDTOs.map(function (delayDTO) {
+            return new google.maps.LatLng(delayDTO.x, delayDTO.y);
+        });
     };
 
-    _this.selectedItemChange = function (item) {
 
-    }
 
-    _this.findDelays = function () {
-
-    }
+    scope.selectedPoints = [ //TODO remove mock
+        new google.maps.LatLng(37.782551, -122.445368),
+        new google.maps.LatLng(37.782745, -122.444586),
+        new google.maps.LatLng(37.782842, -122.443688),
+        new google.maps.LatLng(37.782919, -122.442815),
+        new google.maps.LatLng(37.782992, -122.442112),
+        new google.maps.LatLng(37.783100, -122.441461),
+        new google.maps.LatLng(37.783206, -122.440829),
+        new google.maps.LatLng(37.783273, -122.440324),
+        new google.maps.LatLng(37.783316, -122.440023),
+        new google.maps.LatLng(37.783357, -122.439794),
+        new google.maps.LatLng(37.783371, -122.439687),
+        new google.maps.LatLng(37.783368, -122.439666),
+        new google.maps.LatLng(37.783383, -122.439594),
+        new google.maps.LatLng(37.783508, -122.439525),
+        new google.maps.LatLng(37.783842, -122.439591),
+        new google.maps.LatLng(37.784147, -122.439668),
+        new google.maps.LatLng(37.784206, -122.439686),
+        new google.maps.LatLng(37.784386, -122.439790),
+        new google.maps.LatLng(37.784701, -122.439902),
+        new google.maps.LatLng(37.784965, -122.439938),
+        new google.maps.LatLng(37.785010, -122.439947),
+        new google.maps.LatLng(37.785360, -122.439952),
+        new google.maps.LatLng(37.785715, -122.440030),
+        new google.maps.LatLng(37.786117, -122.440119),
+        new google.maps.LatLng(37.786564, -122.440209),
+        new google.maps.LatLng(37.786905, -122.440270),
+        new google.maps.LatLng(37.786956, -122.440279),
+        new google.maps.LatLng(37.800224, -122.433520),
+        new google.maps.LatLng(37.800155, -122.434101),
+        new google.maps.LatLng(37.800160, -122.434430),
+        new google.maps.LatLng(37.800378, -122.434527),
+        new google.maps.LatLng(37.800738, -122.434598),
+        new google.maps.LatLng(37.800938, -122.434650),
+        new google.maps.LatLng(37.801024, -122.434889),
+        new google.maps.LatLng(37.800955, -122.435392),
+        new google.maps.LatLng(37.800886, -122.435959)
+    ];
 
 }
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -30,9 +30,7 @@ module.exports = function makeWebpackConfig() {
    * Karma will set this when it's a test build
    */
   config.entry = isTest ? void 0 : {
-    app: './src/app/app.js',
-    ngmap: './node_modules/ngmap/build/scripts/ng-map.min.js'
-
+    app: './src/app/app.js'
   };
 
   /**
